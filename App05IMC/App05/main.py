0bef1e15f2c5f4985f1c01e603f44a20a981518e
estefani49 <moraestefani49@gmail.com>
2024-09-30 20:26:41 -0600

    se sube versión 1.1

--- App05IMC/App05/main.py
+++ App05IMC/App05/main.py
@@ -1,5 +1,55 @@
 import flet as ft
 
+def calcular_imc(txtPeso,txtAltura,lblIMC,page):
+    try:
+        peso=float(txtPeso.value)
+        altura=float(txtAltura.value)
+        imc=peso/(altura**2)
+        lblIMC.value=f"Tu IMC es: {imc:.2f}" #el .2f sirve para recorrer dos tantos
+        page.update()
+        
+        #funcion para cerrar el cuadro del dialogo
+        def cerrar_dialogo():
+            page.dialog.open=False
+            page.update()
+            
+        #validar condiciones del IMC
+            if imc<18.5:
+                dialog = ft.AlertDialog(
+                    title=ft.Text("Resultado de IMC"),
+                    content=ft.Text("Actualmente estas bajo de peso"),
+                    actions=[ft.TextButton("OK", on_click=cerrar_dialogo)],
+            )
+            elif imc >=18.5 and imc < 24.9:
+                dialog = ft.AlertDialog(
+                    title=ft.Text("Resultado de IMC"),
+                    content=ft.Text("Actualmente estas bajo de peso"),
+                    actions=[ft.TextButton("OK", on_click=cerrar_dialogo)],
+                )
+            elif imc >= 25 and imc <30:
+                dialog = ft.AlertDialog(
+                    title=ft.Text("Resultado de IMC"),
+                    content=ft.Text("Tienes sobre peso"),
+                    actions=[ft.TextButton("OK", on_click=cerrar_dialogo)],
+                )
+            else:
+                dialog = ft.AlertDialog(
+                    title=ft.Text("Resultado de IMC"),
+                    content=ft.Text("Tienes obesidad"),
+                    actions=[ft.TextButton("OK", on_click=cerrar_dialogo)],
+                )
+            
+    except ValueError:
+    
+        def cerrar_dialogo(e):
+            page.dialog.open=False
+            page.update()
+        
+            dialog=ft.AlertDialog(
+                title=ft.Text("Error"),
+                content=ft.Text("Debes ingresar valores numericos"),
+                actions=[ft.TextButton("OK", on_click=cerrar_dialogo)]
+            )
 
 def main(page: ft.Page):
     page.title = " Calculadora de IMC"
@@ -14,8 +64,20 @@ def main(page: ft.Page):
                 height=22
                 
                 )
-    btnCalcular=ft.ElevatedButton(text="Calcular")
-    btnLimpiar=ft.ElevatedButton(text="Limpiar")
+    
+    def on_calcular_click():
+        calcular_imc(txtPeso,txtAltura,lblIMC,page)
+    
+    
+    def limpiar(e):
+        txtPeso.value=""
+        txtAltura.value=""
+        lblIMC.value="Tu IMC es: "
+        page.update()
+    
+    
+    btnCalcular=ft.ElevatedButton(text="Calcular",on_click=on_calcular_click)
+    btnLimpiar=ft.ElevatedButton(text="Limpiar",on_click=limpiar)
     
     page.add(
         ft.Column(
